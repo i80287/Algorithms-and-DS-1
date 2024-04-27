75dc4b37abcadac559aeaa9d6f7ad41d3d2e4e4c
i80287 <addmlbx@gmail.com>
2024-04-27 15:34:10 +0300

    updated h2 7

--- hw7/c.cpp
+++ hw7/c.cpp
@@ -3,11 +3,9 @@
 #include <cstdint>
 #include <iostream>
 #include <string>
-#include <cstring>
 #include <cassert>
 
 using namespace std;
-
 static vector<size_t> prefix_function(string_view s) {
 	const size_t n = s.length();
 	vector<size_t> pi(n);
@@ -15,10 +13,8 @@ static vector<size_t> prefix_function(string_view s) {
 		size_t j = pi[i - 1];
 		uint32_t c_i = uint8_t(s[i]);
 		while (j > 0 && c_i != uint8_t(s[j])) {
-            assert(j - 1 < n);
             j = pi[j - 1];
         }
-        assert(j < n);
 		if (c_i == uint8_t(s[j])) {
 			j++;
 		}
@@ -35,42 +31,26 @@ static vector<size_t> prefix_function(string_view s) {
 }
 
 int main() {
-    static uint8_t p[size_t(1e3) + 3];
-    static uint8_t t[size_t(1e7) + 3];
-    auto ret = fgets(reinterpret_cast<char*>(p), 1000 + 2, stdin);
-    assert(ret);
-    size_t p_size = strlen(reinterpret_cast<const char*>(p));
-    assert(p_size >= 2);
-    assert(p[p_size - 1] == '\n');
-    p[p_size - 1] = '\0';
-    p_size--;
-    assert(isalpha(p[0]));
-    assert(isalpha(p[p_size - 1]));
-
-    ret = fgets(reinterpret_cast<char*>(t), 10'000'000 + 2, stdin);
-    assert(ret);
-    size_t t_size = strlen(reinterpret_cast<const char*>(t));
-    assert(t_size >= 2);
-    if (t[t_size - 1] == '\n') {
-        t[t_size - 1] = '\0';
-        t_size--;
-    }
-    assert(isalpha(t[0]));
-    assert(isalpha(t[t_size - 1]));
-    // cin >> p >> t;
-    const auto pi = prefix_function(string_view(reinterpret_cast<const char*>(p), p_size));
-    vector<size_t> ans(size_t(1e6) + 10);
-    assert(ans.size() == 1'000'010);
-    size_t ans_size = 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string p;
+    string t;
+    p.reserve(1010);
+    t.reserve(size_t(1e7 + 10));
+    cin >> p >> t;
+    const auto pi = prefix_function(p);
+    vector<size_t> ans;
+    ans.reserve(size_t(1e6) + 10);
     size_t p_i = 0;
     size_t t_i = 0;
-    while (t_i < t_size) {
+    while (t_i < t.size()) {
         if (uint8_t(p[p_i]) == uint8_t(t[t_i])) {
             p_i++;
             t_i++;
-            if (p_i == p_size) {
-                assert(t_i >= p_i);
-                ans[ans_size++] = t_i - p_i;
+            if (p_i == p.size()) [[unlikely]] {
+                assert(t_i >= p.size());
+                ans.push_back(t_i - p_i);
+                p_i = pi[p_i - 1];
             }
         } else if (p_i == 0) {
             t_i++;
@@ -78,8 +58,8 @@ int main() {
             p_i = pi[p_i - 1];
         }
     }
-    printf("%u\n", uint32_t(ans_size));
-    for (size_t i = 0; i < ans_size; i++) {
-        printf("%u\n", uint32_t(ans[i]));
+    cout << ans.size() << '\n';
+    for (size_t i : ans) {
+        cout << uint32_t(i) << '\n';
     }
 }
