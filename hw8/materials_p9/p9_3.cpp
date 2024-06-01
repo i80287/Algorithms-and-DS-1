c0be8023feb0c17869f57ea68ca117d84ee23e98
i80287 <addmlbx@gmail.com>
2024-06-01 11:48:21 +0300

    update hw8

--- hw8/materials_p9/p9_3.cpp
+++ hw8/materials_p9/p9_3.cpp
@@ -1,106 +1,207 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <any>
+#include <array>
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <numbers>
+#include <numeric>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-enum Engine {
-    kNone,
-    kFirst,
+inline constexpr size_t n             = 2;
+inline constexpr size_t m             = 2;
+inline constexpr size_t kMaxW         = 85;
+inline constexpr array<uint32_t, 5> p = {1, 2, 3, 4, 5};
+
+enum class Engine {
+    kNone  = -1,
+    kFirst = 0,
     kSecond,
     kThird,
     kFourth,
     kFifth,
 };
+static_assert(uint32_t(Engine::kFirst) == 0 && uint32_t(Engine::kFifth) == 4);
 
-inline constexpr size_t n     = 6;
-inline constexpr size_t m     = 5;
-inline constexpr uint32_t max_w = 85;
-inline constexpr array<uint32_t, 5> p = {1, 2, 3, 4, 5};
-static constinit array<array<Engine, m>, n> dp{};
-static_assert(static_cast<int>(Engine::kNone) == 0);
+consteval array<array<Engine, m>, n> init_dp() noexcept {
+    array<array<Engine, m>, n> arr{};
+    for (auto& r : arr) {
+        r.fill(Engine::kNone);
+    }
+    return arr;
+}
+static constinit array<array<Engine, m>, n> dp = init_dp();
 
-static vector<pair<size_t, size_t>> hist_stack;
+struct coord_t final {
+    size_t i = size_t(-1);
+    size_t j = size_t(-1);
+};
 
-static pair<size_t, size_t> get_next_coord() {
-    static size_t i = 0;
-    static size_t j = 0;
-    pair ans = {i, j};
+static vector<coord_t> hist_stack;
+static size_t g_i = 0;
+static size_t g_j = 0;
+
+static coord_t get_next_coords() {
+    const coord_t ans = {.i = g_i, .j = g_j};
     hist_stack.push_back(ans);
-    if (i == 0) [[unlikely]] {
-        i = min(j + 1, n - 1);
-        j = j + 1 - i; 
-    } else if (j == m - 1) [[unlikely]] {
-        j = m - n + 1 + i;
-        i = n - 1;
+    if (g_i == 0) [[unlikely]] {
+        g_i = min(g_j + 1, n - 1);
+        g_j = g_j + 1 - g_i;
+    } else if (g_j == m - 1) [[unlikely]] {
+        g_j = m - n + 1 + g_i;
+        g_i = n - 1;
     } else {
-        i--;
-        j++;
+        g_i--;
+        g_j++;
     }
     return ans;
 }
 
-struct coord_t { size_t i = size_t(-1); size_t j = size_t(-1); };
-static deque<pair<coord_t, Engine>> q;
-static constinit array<array<uint8_t, m>, n> visited{};
-
-static int64_t check_map(const size_t i0, const size_t j0) {
-    for (size_t i = size_t(max(int64_t(0), int64_t(i0 - 1))); i < min(n, i0 + 2); i++) {
-        for (size_t j = size_t(max(int64_t(0), int64_t(j0 - 1))); j < min(m, j0 + 2); j++) {
-            auto update_mask = [&](uint32_t& mask, size_t i, size_t j) constexpr noexcept {
-                if (i < n && j < m) {
-                    mask |= 1u << dp[i][j];
-                }
-            };
-            uint32_t mask = 0;
-            update_mask(mask, i - 1, j);
-            update_mask(mask, i + 1, j);
-            update_mask(mask, i, j - 1);
-            update_mask(mask, i, j + 1);
-            mask >>= 1;
-            const Engine max_engine = Engine(countr_one(mask) + 1);
-            if (dp[i][j] > max_engine) {
-                return false;
-            }
-        }
-    }
-
-    return true;
+static void pop_coords() noexcept {
+    assert(!hist_stack.empty());
+    const auto [last_i, last_j] = hist_stack.back();
+    g_i                         = last_i;
+    g_j                         = last_j;
+    hist_stack.pop_back();
 }
 
-static bool visit(size_t i, size_t j, Engine e) {
-    dp[i][j] = e;
-    visited[i][j] = true;
+inline constexpr std::array<uint32_t, p.size()> p_count_boundaries = {
+    std::min(n * m, kMaxW / p[0]), std::min(n * m, kMaxW / p[1]), std::min(n * m, kMaxW / p[2]),
+    std::min(n * m, kMaxW / p[3]),  std::min(n * m, kMaxW / p[4]),
+};
+static constinit std::array<uint32_t, p.size()> p_count{};
+static int64_t current_w = 0;
 
-    int64_t sm = check_map();
-    if (sm < 0) {
-        dp[i][j] = kNone;
-        visited[i][j] = false;
-        return false;
+static bool update_map(const size_t i0, const size_t j0, Engine e) noexcept {
+    auto check_cell = [](size_t i, size_t j) constexpr noexcept {
+        if (i >= n || j >= m || dp[i][j] == Engine::kNone) {
+            return true;
+        }
+
+        auto update_mask = [](uint32_t& mask, size_t i1, size_t j1) constexpr noexcept {
+            if (i1 >= n || j1 >= m) {
+                return false;
+            }
+            const auto d = dp[i1][j1];
+            if (d == Engine::kNone) {
+                return true;
+            }
+            assert(Engine::kFirst <= d && d <= Engine::kFifth);
+            size_t idx = uint32_t(d);
+            mask |= 1u << idx;
+            return false;
+        };
+
+        uint32_t mask = 0;
+        if (update_mask(mask, i - 1, j) || update_mask(mask, i + 1, j) ||
+            update_mask(mask, i, j - 1) || update_mask(mask, i, j + 1)) {
+            return true;
+        }
+        const Engine max_engine = Engine(std::countr_one(mask));
+        return dp[i][j] <= max_engine;
+    };
+
+    dp[i0][j0] = e;
+    const bool adj_cells_check = check_cell(i0, j0 - 1) & check_cell(i0, j0 + 1) &
+                                 check_cell(i0 - 1, j0) & check_cell(i0 + 1, j0) &
+                                 check_cell(i0, j0);
+    assert(Engine::kFirst <= e && e <= Engine::kFifth);
+    size_t idx = uint32_t(e);
+    assert(idx < p.size());
+    current_w += p[idx];
+    const bool b1 = current_w <= int64_t(kMaxW);
+    const bool b2 = ++p_count[idx] <= p_count_boundaries[idx];
+    return adj_cells_check && b1 && b2;
+}
+
+static void restore_map(size_t i, size_t j) {
+    const Engine e = dp[i][j];
+    dp[i][j] = Engine::kNone;
+    assert(Engine::kFirst <= e && e <= Engine::kFifth);
+    size_t idx = uint32_t(e);
+    assert(idx < p.size());
+    assert(current_w >= p[idx]);
+    current_w -= p[idx];
+    assert(p_count[idx] > 0);
+    --p_count[idx];
+}
+
+static int64_t max_ans = -1;
+
+static int64_t calculate_map() noexcept {
+    int64_t ans = 0;
+    for (const auto& r : dp) {
+        for (Engine e : r) {
+            size_t idx = uint32_t(e);
+            assert(idx < p.size());
+            ans += p[idx];
+        }
+    }
+    return ans;
+}
+
+static void print_map(const size_t i, const size_t j, const Engine e) {
+    cout << "Successfully assigned " << uint32_t(e) << " at position " << i << ' ' << j << '\n';
+    for (const auto& r : dp) {
+        for (Engine e1 : r) {
+            cout << int(e1) << ' ';
+        }
+        cout << '\n';
+    }
+    cout << "\n\n";
+}
+
+static bool visit(const size_t i, const size_t j, const Engine e) {
+    assert(i < n && j < m);
+    assert(dp[i][j] == Engine::kNone);
+    assert(Engine::kFirst <= e && e <= Engine::kFifth);
+
+    bool check_res = update_map(i, j, e);
+    if (check_res) {
+        print_map(i, j, e);
+
+        const auto [ni, nj] = get_next_coords();
+        if (ni < n && nj < m) [[likely]] {
+            for (uint32_t e1 = uint32_t(Engine::kFirst); e1 <= uint32_t(Engine::kFifth); e1++) {
+                if (!visit(ni, nj, Engine(e1))) {
+                    // if update_map for {ni,nj,e1} if false, it will be false for all higher engines
+                    break;
+                }
+            }
+        } else {
+            assert(current_w == calculate_map());
+            max_ans = max(max_ans, current_w);
+        }
+        pop_coords();
     }
 
-    const pair<uint32_t, uint32_t> nbrs[] = {{i - 1, j}, {i + 1, j}, {i, j - 1}, {i, j + 1}};
-    for (uint32_t e1 = kFirst; e1 <= kFifth; e1++) {
-        for (const auto [ni, nj] : nbrs) {
-            if (ni < n && nj < m && !visited[i][j]) {
-                visit(ni, nj, Engine(e1));
-            }
-        }
+    restore_map(i, j);
+    return check_res;
+}
+
+static void solve() {
+    hist_stack.reserve(n * m);
+    for (uint32_t e1 = uint32_t(Engine::kFirst); e1 <= uint32_t(Engine::kFifth); e1++) {
+        const auto [ni, nj] = get_next_coords();
+        assert(ni == 0 && nj == 0);
+        visit(ni, nj, Engine(e1));
+        pop_coords();
     }
 }
 
 int main() {
-    array<array<int32_t, m>, n> mp{};
-    memset(&mp, -1, sizeof(mp));
-    for (int32_t k = 0; k < n * m; k++) {
-        auto [i, j] = get_next_coord();
-        assert(i < n && j < m);
-        mp[i][j] = k;
-    }
-    auto [a, b] = get_next_coord();
-    cout << a << ' ' << b << "\n\n";
-    for (uint32_t i = 0; i < n; i++) {
-        for (uint32_t j = 0; j < m; j++) {
-            cout << setw(3) << mp[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+    solve();
+    cout << max_ans;
 }
---
+++ hw8/materials_p9/p9_4.cpp
@@ -0,0 +1,233 @@
+#include <algorithm>
+#include <any>
+#include <array>
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <numbers>
+#include <numeric>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+inline constexpr size_t n             = 5;
+inline constexpr size_t m             = 5;
+inline constexpr size_t kMaxW         = 85;
+inline constexpr array<uint32_t, 6> p = {0, 1, 2, 3, 4, 5};
+
+enum class Engine : uint32_t {
+    kNone = 0,
+    kFirst,
+    kSecond,
+    kThird,
+    kFourth,
+    kFifth,
+};
+// static_assert(uint32_t(Engine::kFirst) == 0 && uint32_t(Engine::kFifth) == 4);
+static_assert(uint32_t(Engine::kNone) >= p.size() || p[uint32_t(Engine::kNone)] == 0);
+
+consteval array<array<Engine, m>, n> init_dp() noexcept {
+    array<array<Engine, m>, n> arr{};
+    for (auto& r : arr) {
+        r.fill(Engine::kNone);
+    }
+    return arr;
+}
+static constinit array<array<Engine, m>, n> dp = init_dp();
+
+inline constexpr std::array<uint64_t, p.size()> p_count_boundaries = {
+    n * m,
+    std::min(n* m, kMaxW / p[uint32_t(Engine::kFirst)]),
+    std::min(n* m, kMaxW / p[uint32_t(Engine::kSecond)]),
+    std::min(n* m, kMaxW / p[uint32_t(Engine::kThird)]),
+    std::min(n* m, kMaxW / p[uint32_t(Engine::kFourth)]),
+    std::min(n* m, kMaxW / p[uint32_t(Engine::kFifth)]),
+};
+static constinit std::array<uint64_t, p.size()> p_count = {n * m, 0, 0, 0, 0, 0};
+static constinit uint64_t current_w                     = n * m * p[0];
+static constinit uint64_t max_ans                       = n * m * p[0];
+static constinit uint64_t valid_taken                   = 0;
+
+struct coord_t final {
+    size_t i                                                 = size_t(-1);
+    size_t j                                                 = size_t(-1);
+    constexpr bool operator==(const coord_t&) const noexcept = default;
+};
+
+static Engine exchange_cell(size_t i, size_t j, Engine new_e) {
+    const Engine old_e = dp[i][j];
+    dp[i][j]           = new_e;
+    assert(Engine::kNone <= new_e && new_e <= Engine::kFifth);
+    assert(Engine::kNone <= old_e && old_e <= Engine::kFifth);
+    size_t idx_new = uint32_t(new_e);
+    size_t idx_old = uint32_t(old_e);
+    assert(idx_new < p.size());
+    assert(idx_old < p.size());
+    assert(current_w >= p[idx_old]);
+    current_w -= p[idx_old];
+    current_w += p[idx_new];
+    assert(p_count[idx_old] > 0);
+    --p_count[idx_old];
+    ++p_count[idx_new];
+    valid_taken -= old_e != Engine::kNone;
+    assert(int64_t(valid_taken) >= 0);
+    valid_taken += new_e != Engine::kNone;
+    return old_e;
+}
+
+static std::pair<bool, Engine> update_map(const size_t i0, const size_t j0,
+                                          const Engine e) noexcept {
+    auto check_cell = [](size_t i, size_t j) constexpr noexcept {
+        if (i >= n || j >= m) {
+            return true;
+        }
+
+        auto update_mask = [](uint32_t& mask, const size_t i1, const size_t j1) constexpr noexcept {
+            if (i1 >= n || j1 >= m) [[unlikely]] {
+                return;
+            }
+            const Engine d = dp[i1][j1];
+            assert(Engine::kNone <= d && d <= Engine::kFifth);
+            const size_t idx = uint32_t(d);
+            mask |= 1u << idx;
+        };
+
+        uint32_t mask = 0;
+        update_mask(mask, i - 1, j);
+        update_mask(mask, i + 1, j);
+        update_mask(mask, i, j - 1);
+        update_mask(mask, i, j + 1);
+        mask >>= 1;
+        const Engine max_engine = Engine(std::countr_one(mask) + 1);
+        return dp[i][j] <= max_engine;
+    };
+
+    const Engine old_e         = exchange_cell(i0, j0, e);
+    const bool adj_cells_check = check_cell(i0, j0 - 1) & check_cell(i0, j0 + 1) &
+                                 check_cell(i0 - 1, j0) & check_cell(i0 + 1, j0) &
+                                 check_cell(i0, j0);
+    assert(Engine::kFirst <= e && e <= Engine::kFifth);
+    const size_t idx = uint32_t(e);
+    assert(idx < p.size());
+    const bool b1 = current_w <= kMaxW;
+    const bool b2 = p_count[idx] <= p_count_boundaries[idx];
+
+    return {adj_cells_check && b1 && b2, old_e};
+}
+
+static uint64_t calculate_map() noexcept {
+    uint64_t ans = 0;
+    for (const auto& r : dp) {
+        for (Engine e : r) {
+            const size_t idx = uint32_t(e);
+            assert(idx < p.size());
+            ans += p[idx];
+        }
+    }
+    return ans;
+}
+
+static void print_map(const size_t i, const size_t j, const Engine e) {
+    cout << "Successfully assigned " << uint32_t(e) << " at position " << i << ' ' << j << '\n';
+    for (const auto& r : dp) {
+        for (Engine e1 : r) {
+            cout << int(e1) << ' ';
+        }
+        cout << '\n';
+    }
+    cout << "\n\n";
+}
+
+static vector<coord_t> hist;
+
+static bool acquire_hist(const size_t i, const size_t j) {
+    const coord_t c{i, j};
+    const size_t sz = hist.size();
+    for (int64_t k = int64_t(sz - 1); k >= std::max(int64_t(0), int64_t(sz - 3)); k--) {
+        assert(size_t(k) < hist.size());
+        if (hist[size_t(k)] == c) {
+            return false;
+        }
+    }
+    hist.push_back(c);
+    return true;
+}
+static void release_hist([[maybe_unused]] const size_t i,
+                         [[maybe_unused]] const size_t j) noexcept {
+    assert((!hist.empty() && hist.back() == coord_t{i, j}));
+    hist.pop_back();
+}
+
+static bool visit(const size_t i, const size_t j, const Engine e) {
+    if (dp[i][j] == e) {
+        return true;
+    }
+    if (!acquire_hist(i, j)) {
+        return false;
+    }
+
+    assert(i < n && j < m);
+    assert(Engine::kFirst <= e && e <= Engine::kFifth);
+
+    const auto [check_res, old_e] = update_map(i, j, e);
+    if (check_res) {
+        print_map(i, j, e);
+
+        assert(current_w == calculate_map());
+        max_ans              = max(max_ans, current_w);
+        const coord_t nbrs[] = {
+            {.i = i, .j = j - 1}, {.i = i, .j = j + 1}, {.i = i - 1, .j = j}, {.i = i + 1, .j = j}};
+        for (const auto [ni, nj] : nbrs) {
+            if (ni >= n || nj >= m) [[unlikely]] {
+                continue;
+            }
+            for (uint32_t e1 = uint32_t(Engine::kFirst); e1 <= uint32_t(Engine::kFifth); e1++) {
+                if (!visit(ni, nj, Engine(e1))) {
+                    // if update_map for {ni,nj,e1} if false,
+                    //  it will be false for all higher engines.
+                    break;
+                }
+            }
+        }
+    }
+
+    [[maybe_unused]] const Engine e1 = exchange_cell(i, j, old_e);
+    assert(e1 == e);
+    release_hist(i, j);
+    return check_res;
+}
+
+static void solve() {
+    hist.reserve(4 * n * m);
+    for (uint32_t e1 = uint32_t(Engine::kFirst); e1 <= uint32_t(Engine::kFifth); e1++) {
+        visit(0, 0, Engine(e1));
+    }
+}
+
+#include <sys/resource.h>
+
+int main() {
+    rlimit rl{};
+    if (getrlimit(RLIMIT_STACK, &rl)) {
+        perror("getrlimit");
+        return 1;
+    }
+    rl.rlim_cur = 32u << 20;
+    if (setrlimit(RLIMIT_STACK, &rl)) {
+        perror("setrlimit");
+        return 1;
+    }
+
+    solve();
+    cout << max_ans;
+}
